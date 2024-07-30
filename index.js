e5bb6b8dccc7da6be93ee1400ea961a10bab789e
unknown <vargasparrakevin@gmail.com>
2024-07-30 17:55:06 -0500

    game

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+const port = process.env.PORT||8000
+const path = require('path');
+const PlayerIO=require("./pio")
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+
+
+
+PlayerIO.authenticate(
+    'esp32-6zcecyhrgeolz15gxqwxmw',
+    "dcfdgteynoifyek",
+    {
+        username:'__admin345__fzfqmmt0woxqmamv86fa',password:"fzfqmmt0woxqmamv86faj2912994nn",
+        //register: true
+    },
+    null,
+    function(client) {
+        console.log("inicio")
+
+
+
+
+        app.get('/:query/:name',(req,res)=>{
+            client.bigDB.loadOrCreate("code", req.params.name, function(obj) {
+                if(!obj||!obj[req.params.query]){
+                    res.send("")
+                }
+                res.send(""+obj[req.params.query])
+            }, function(error) { console.log(error);res.send("") });
+        })
+        app.post('/save/:name',(req,res)=>{
+            client.bigDB.loadOrCreate("code", req.params.name, function(obj) {
+                if(!obj.version){
+                    obj.version=0
+                }
+                obj.version++
+                obj.code=req.body.code
+                obj.save()
+                res.send("ok")
+            }, function(error) {res.send(error) });
+        })
+        
+
+
+
+
+    },
+    function(error) {
+        if (error.code == PlayerIOErrorCode.UnknownGame) {
+        } else {
+            console.log(error);
+        }
+    }
+);
+
+
+
+
+
+app.get('/', (req, res) => {
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ upload.bat
@@ -0,0 +1,4 @@
+CALL git add .
+CALL git commit -m "game"
+CALL git push
+pause
